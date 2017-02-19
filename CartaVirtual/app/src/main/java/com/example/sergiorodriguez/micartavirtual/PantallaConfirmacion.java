484f55c867de5d77f089c6b6b032312c95dea87c
SergioRodriguez317 <sergioarg17@gmail.com>
2017-02-19 00:56:51 -0500

    Mi Carta Virtual- Pantalla Confirmacion

--- CartaVirtual/app/src/main/java/com/example/sergiorodriguez/micartavirtual/PantallaConfirmacion.java
+++ CartaVirtual/app/src/main/java/com/example/sergiorodriguez/micartavirtual/PantallaConfirmacion.java
@@ -4,15 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class PantallaConfirmacion extends AppCompatActivity {
 
-    PantallaReserva nombreCliente=new PantallaReserva();
-    PantallaReserva documentoCliente=new PantallaReserva();
-    PantallaReserva telefonoCliente=new PantallaReserva();
-    PantallaReserva fechaReserva=new PantallaReserva();
-    PantallaReserva horaReserva=new PantallaReserva();
-    PantallaReserva cantidadPersonas=new PantallaReserva();
+    TextView nombreCliente,documento,telefono,fecha,hora,cantidad;
 
 
 
@@ -20,6 +16,33 @@ public class PantallaConfirmacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_confirmacion);
+
+        nombreCliente=(TextView)findViewById(R.id.nombreCliente);
+        documento=(TextView) findViewById(R.id.documentoCliente);
+        telefono=(TextView) findViewById(R.id.telefonoCliente);
+        fecha=(TextView) findViewById(R.id.fechaReserva);
+        hora=(TextView) findViewById(R.id.horaReserva);
+        cantidad=(TextView) findViewById(R.id.cantidadPersonas);
+
+        Intent intencion=getIntent();
+        Bundle extras=intencion.getExtras();
+
+        if(extras!=null)
+        {
+            String datoNombre=extras.getString("DATO");
+            String datoDocumento=extras.getString("DatoDocumento");
+            String datoTelefono=extras.getString("DatoTelefono");
+            String datoFecha=extras.getString("DatoFecha");
+            String datoHora=extras.getString("DatoHora");
+            String datoCantidad=extras.getString("DatoCantidad");
+            nombreCliente.setText(datoNombre);
+            documento.setText(datoDocumento);
+            telefono.setText(datoTelefono);
+            fecha.setText(datoFecha);
+            hora.setText(datoHora);
+            cantidad.setText(datoCantidad);
+
+        }
     }
     public void OnClickBotonAtras(View v){
         Intent intencion=new Intent(getApplicationContext(),PantallaReserva.class);
--- CartaVirtual/app/src/main/java/com/example/sergiorodriguez/micartavirtual/PantallaReserva.java
+++ CartaVirtual/app/src/main/java/com/example/sergiorodriguez/micartavirtual/PantallaReserva.java
@@ -5,14 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class PantallaReserva extends AppCompatActivity {
 
+    EditText texto_Nombre,documento,telefono,fecha,hora,cantidad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_reserva);
+
+        texto_Nombre=(EditText)findViewById(R.id.texto_Nombre);
+        documento=(EditText)findViewById(R.id.numeroDocumento);
+        fecha=(EditText)findViewById(R.id.fechaReserva);
+        hora=(EditText)findViewById(R.id.horaReserva);
+        telefono=(EditText)findViewById(R.id.telefono);
+        cantidad=(EditText)findViewById(R.id.cantidadPersonas);
 
         Spinner prueba = (Spinner) findViewById(R.id.tipoDocumento);
         //Creamos el adaptador
@@ -28,7 +38,20 @@ public class PantallaReserva extends AppCompatActivity {
         startActivity(intencion);
     }
     public void OnClickBotonConfirmar(View v){
+
+        String datoNombre=texto_Nombre.getText().toString();
+        String datoDocumento=documento.getText().toString();
+        String datoFecha=fecha.getText().toString();
+        String datoHora=hora.getText().toString();
+        String datoTelefono=telefono.getText().toString();
+        String datoCantidad=cantidad.getText().toString();
         Intent intencion=new Intent(getApplicationContext(),PantallaConfirmacion.class);
+        intencion.putExtra("DATO",datoNombre);
+        intencion.putExtra("DatoDocumento",datoDocumento);
+        intencion.putExtra("DatoFecha",datoFecha);
+        intencion.putExtra("DatoHora",datoHora);
+        intencion.putExtra("DatoTelefono",datoTelefono);
+        intencion.putExtra("DatoCantidad",datoCantidad);
         startActivity(intencion);
     }
 }
